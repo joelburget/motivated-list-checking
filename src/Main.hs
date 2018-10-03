7a7e4f191e6782c7a0e35b224888c718860a836e
Joel Burget <joelburget@gmail.com>
2018-10-03 14:44:51 -0700

    Working on smashing together motives and evidence.

--- src/Main.hs
+++ src/Main.hs
@@ -15,8 +15,6 @@
 module Main where
 
 import Prelude as P hiding (concat)
-import Data.Foldable (for_)
-import Control.Lens hiding (List, (.>))
 import Control.Monad
 import Data.SBV
 import           Data.SBV.List ((.++), (.!!))
@@ -25,9 +23,10 @@ import qualified Data.SBV.List as SBVL
 
 -- A list represented as the result of a fold
 newtype FoldedList a = FoldedList { unFoldedList :: a }
-  deriving Functor
+  deriving (Show, Functor)
 
 newtype SListLength = SListLength { unSListLength :: SBV Integer }
+  deriving Show
 
 instance Semigroup SListLength where
   SListLength x <> SListLength y = SListLength $ x + y
@@ -36,6 +35,7 @@ instance Monoid SListLength where
   mempty = SListLength 0
 
 newtype SAll = SAll { unSAll :: SBV Bool }
+  deriving Show
 
 instance Semigroup SAll where
   SAll x <> SAll y = SAll (x &&& y)
@@ -44,12 +44,18 @@ instance Monoid SAll where
   mempty = SAll true
 
 newtype SAny = SAny { unSAny :: SBV Bool }
+  deriving Show
 
 instance Semigroup SAny where
   SAny x <> SAny y = SAny (x ||| y)
 
 instance Monoid SAny where
   mempty = SAny false
+
+data SCmp = SCmp Ordering (SBV Integer)
+  deriving Show
+
+-- TODO: contains monoid
 
 cons :: Monoid a => a -> FoldedList a -> FoldedList a
 cons a (FoldedList f) = FoldedList $ a <> f
@@ -80,56 +86,98 @@ type Suitable a = (Show (Concrete a), SymWord (Concrete a))
 
 data Expr ty where
   -- list consumers
-  ListLen :: Suitable a
-          => Expr ('List a)                   -> Expr 'IntTy
-  ListAnd :: Expr ('List 'BoolTy)             -> Expr 'BoolTy
-  ListOr  :: Expr ('List 'BoolTy)             -> Expr 'BoolTy
-  ListEq  :: Suitable a
-          => Expr ('List a) -> Expr ('List a) -> Expr 'BoolTy
-  ListAt  :: Expr ('List a) -> Expr 'IntTy    -> Expr a
+  ListLen  :: Suitable a
+           => Expr ('List a)                   -> Expr 'IntTy
+  ListAnd  :: Expr ('List 'BoolTy)             -> Expr 'BoolTy
+  ListOr   :: Expr ('List 'BoolTy)             -> Expr 'BoolTy
+  ListEq   :: Suitable a
+           => Expr ('List a) -> Expr ('List a) -> Expr 'BoolTy
+  ListAt   :: Expr ('List a) -> Expr 'IntTy    -> Expr a
   ListContains
-          :: Suitable a
-          => Expr ('List a) -> Expr a    -> Expr 'BoolTy
+           :: Suitable a
+           => Expr ('List a) -> Expr a         -> Expr 'BoolTy
 
   -- transducers
-  ListCat :: Suitable a
-          => Expr ('List a) -> Expr ('List a) -> Expr ('List a)
-  ListMap :: Suitable a
-          => (     Concrete a  ->      Concrete b)
-          -> (SBV (Concrete a) -> SBV (Concrete b))
-          ->                   Expr ('List a) -> Expr ('List b)
+  ListCat  :: Suitable a
+           => Expr ('List a) -> Expr ('List a) -> Expr ('List a)
+  ListMap  :: Suitable a
+           => (     Concrete a  ->      Concrete b)
+           -> (SBV (Concrete a) -> SBV (Concrete b))
+           ->                   Expr ('List a) -> Expr ('List b)
 
   -- producers
-  Lit     :: Concrete a                       -> Expr a
-  Sym     :: SBV (Concrete a)                 -> Expr a
+  Sym      :: SBV (Concrete a)                 -> Expr a
+  ListInfo :: ListInfo a                       -> Expr a
 
   -- other
-  Eq      :: (Eq (Concrete a), Suitable a)
-          => Expr a         -> Expr a         -> Expr 'BoolTy
-  Gt      :: Expr 'IntTy    -> Expr 'IntTy    -> Expr 'BoolTy
-  And     :: Expr 'BoolTy   -> Expr 'BoolTy   -> Expr 'BoolTy
-  Or      :: Expr 'BoolTy   -> Expr 'BoolTy   -> Expr 'BoolTy
-  Not     ::                   Expr 'BoolTy   -> Expr 'BoolTy
+  LitB     :: Concrete 'BoolTy                 -> Expr 'BoolTy
+  LitI     :: Concrete 'IntTy                  -> Expr 'IntTy
+  Eq       :: (Eq (Concrete a), Suitable a)
+           => Expr a         -> Expr a         -> Expr 'BoolTy
+  Gt       :: Expr 'IntTy    -> Expr 'IntTy    -> Expr 'BoolTy
+  And      :: Expr 'BoolTy   -> Expr 'BoolTy   -> Expr 'BoolTy
+  Or       :: Expr 'BoolTy   -> Expr 'BoolTy   -> Expr 'BoolTy
+  Not      ::                   Expr 'BoolTy   -> Expr 'BoolTy
+
+data ListInfo a where
+  LitList :: [SBV (Concrete a)] -> ListInfo a
+  LenInfo :: SListLength        -> ListInfo a
+  AllInfo :: SAll               -> ListInfo a
+  AnyInfo :: SAny               -> ListInfo a
+  CmpInfo :: SCmp               -> ListInfo a
+  deriving Show
 
 instance Show (Expr ty) where
-  show = \case
-    ListLen l          -> "ListLen"
-    ListAnd l          -> "ListAnd"
-    ListOr  l          -> "ListOr"
-    ListEq  a b        -> "ListEq"
-    ListAt lst i       -> "ListAt"
-    ListContains lst a -> "ListContains"
+  showsPrec p expr = showParen (p > 10) $ case expr of
+    ListLen l          -> showString "ListLen" . showsPrec 10 l
+    ListAnd l          -> showString "ListAnd" . showsPrec 10 l
+    ListOr  l          -> showString "ListOr" . showsPrec 10 l
+    ListEq  a b        -> showString "ListEq" . showsPrec 10 a . showsPrec 10 b
+    ListAt lst i       ->
+      showString "ListAt " .
+      showsPrec 10 lst .
+      showString " " .
+      showsPrec 10 i
+    ListContains lst a ->
+      showString "ListContains " .
+      showsPrec 10 lst .
+      showString " " .
+      showsPrec 10 a
 
-    ListCat a b        -> "ListCat"
-    ListMap f _ as     -> "ListMap"
+    ListCat a b        ->
+      showString "ListCat " .
+      showsPrec 10 a .
+      showString " " .
+      showsPrec 10 b
+    ListMap _ _ as     ->
+      showString "ListMap _ _ " .
+      showsPrec 10 as
+    ListInfo i         -> showString "ListInfo " . showsPrec 10 i
 
-    Eq a b             -> "Eq"
-    Gt a b             -> "Gt"
-    And a b            -> "And"
-    Or a b             -> "Or"
-    Not a              -> "Not"
-    Lit a              -> "Lit"
-    Sym _              -> "Sym"
+    LitB a             -> showString "LitB" . showsPrec 10 a
+    LitI a             -> showString "LitI" . showsPrec 10 a
+    Eq a b             ->
+      showString "Eq " .
+      showsPrec 10 a .
+      showString " " .
+      showsPrec 10 b
+    Gt a b             ->
+      showString "Gt" .
+      showsPrec 10 a .
+      showString " " .
+      showsPrec 10 b
+    And a b            ->
+      showString "And" .
+      showsPrec 10 a .
+      showString " " .
+      showsPrec 10 b
+    Or a b             ->
+      showString "Or" .
+      showsPrec 10 a .
+      showString " " .
+      showsPrec 10 b
+    Not a              -> showString "Not" . showsPrec 10 a
+    Sym _              -> showString "Sym _"
 
 eval :: Expr ty -> Concrete ty
 eval = \case
@@ -142,13 +190,15 @@ eval = \case
 
   ListCat a b        -> eval a <> eval b
   ListMap f _ as     -> f <$> eval as
+  ListInfo _         -> error "cannot evaluate list info"
 
   Eq a b             -> eval a == eval b
   Gt a b             -> eval a >  eval b
   And a b            -> eval a && eval b
   Or a b             -> eval a || eval b
   Not a              -> not (eval a)
-  Lit a              -> a
+  LitB a             -> a
+  LitI a             -> a
   Sym _              -> error "cannot evaluate symbolic value"
 
 sEval :: SymWord (Concrete ty) => Expr ty -> SBV (Concrete ty)
@@ -162,13 +212,15 @@ sEval = \case
 
   ListCat a b    -> sEval a .++ sEval b
   ListMap{}      -> error "can't map with sbv lists"
+  ListInfo _     -> error "can't evaluate list info"
 
   Eq a b         -> sEval a .== sEval b
   Gt a b         -> sEval a .>  sEval b
   And a b        -> sEval a &&& sEval b
   Or a b         -> sEval a ||| sEval b
   Not a          -> bnot (sEval a)
-  Lit a          -> literal a
+  LitB a         -> literal a
+  LitI a         -> literal a
   Sym a          -> a
 
 -- "My claim is that we should exploit a hypothesis not in terms of its
@@ -196,7 +248,9 @@ evalMotive' (Length len) = \case
     evalMotive' (Length bl) b
   ListMap _ _ lst -> evalMotive' (Length len) lst
   ListAt{} -> error "nested lists not allowed"
-  Lit lst     -> constrain $ fromIntegral (length lst) .== len
+  ListInfo i -> case i of
+    LenInfo (SListLength len') -> constrain $ len' .== len
+    _                          -> error $ "sorry, can't help with this motive: " ++ show i
   Sym lst     -> constrain $ SBVL.length lst .== len
 evalMotive' (MAnd f) = \case
   ListCat a b -> do
@@ -204,11 +258,13 @@ evalMotive' (MAnd f) = \case
     evalMotive' (MAnd f) b
   ListMap _ g lst -> evalMotive' (MAnd (f . g)) lst
   ListAt{} -> error "nested lists not allowed"
-  Lit lst  ->
-    constrain $ foldr (&&&) true (f . literal <$> lst)
+  ListInfo i -> case i of
+    AllInfo (SAll b) -> constrain $ error "TODO"
+    -- constrain $ foldr (&&&) true (f . literal <$> lst)
     -- traverse (constrain . f . literal) lst >> pure ()
+    _ -> error $ "sorry, can't help with this motive: " ++ show i
   Sym lst -> do
-    i <- forall_
+    i <- forall "i"
     constrain $ i .>= 0 &&& i .< SBVL.length lst ==> f (lst .!! i)
 evalMotive' (MOr f) = \case
   ListCat a b -> do
@@ -216,17 +272,19 @@ evalMotive' (MOr f) = \case
     evalMotive' (MOr f) b
   ListMap _ g lst -> evalMotive' (MOr (f . g)) lst
   ListAt{} -> error "nested lists not allowed"
-  Lit lst  -> constrain $ foldr (|||) false (f . literal <$> lst)
+  ListInfo lst  -> error "TODO"
+    -- constrain $ foldr (|||) false (f . literal <$> lst)
   Sym lst -> do
-    i <- exists_
+    i <- exists "i"
     constrain $ i .>= 0 &&& i .< SBVL.length lst ==> f (lst .!! i)
 evalMotive' (MEq lst) = \case
   ListCat a b -> constrain $ sEval a .++ sEval b .== sEval lst
   ListMap{} -> error "XXX tricky"
   ListAt{} -> error "nested lists not allowed"
-  Lit litLst  -> do
-    ifor_ litLst $ \i val -> constrain $
-      sEval lst .!! fromIntegral i .== literal val
+  ListInfo litLst  -> error "TODO"
+    -- do
+    -- ifor_ litLst $ \i val -> constrain $
+    --   sEval lst .!! fromIntegral i .== literal val
   Sym _       -> error "can't eliminate a symbolic list 2"
 evalMotive' (MAt i a) = \case
   ListCat l1 l2 -> do
@@ -237,8 +295,9 @@ evalMotive' (MAt i a) = \case
       l2' .!! (i - SBVL.length l1') .== a
   ListMap{} -> error "XXX tricky 3"
   ListAt{} -> error "nested lists not allowed"
-  Lit litLst -> ifor_ litLst $ \j val -> constrain $
-    fromIntegral j .== i ==> literal val .== a
+  ListInfo litLst -> error "TODO"
+  -- ifor_ litLst $ \j val -> constrain $
+  --   fromIntegral j .== i ==> literal val .== a
   Sym _ -> error "can't eliminate a symbolic list 3"
 evalMotive' motive@(MContains a) = \case
   ListCat l1 l2 -> do
@@ -246,8 +305,9 @@ evalMotive' motive@(MContains a) = \case
     evalMotive' motive l2
   ListMap{} -> error "XXX tricky 4"
   ListAt{} -> error "nested lists not allowed"
-  Lit litLst -> for_ litLst $ \val -> constrain $
-    literal val .== a
+  ListInfo litLst -> error "TODO"
+  -- for_ litLst $ \val -> constrain $
+  --   literal val .== a
   Sym _ -> error "TODO"
 
 evalMotive :: Suitable ty => SBV (Concrete ty) -> Expr ty -> Symbolic ()
@@ -292,25 +352,17 @@ main = do
     [a, b] <- sIntegers ["a", "b"]
     let l :: Expr ('List 'IntTy)
         l = Sym (SBVL.implode [a, b])
-    pure $ sEval $ Eq (Lit 2) (ListLen l)
+    pure $ sEval $ Eq (LitI 2) (ListLen l)
 
   makeReport "length [] == 0 (expect good)" $ do
-    let lst = Lit [] :: Expr ('List 'IntTy)
+    let lst = ListInfo (LenInfo (SListLength 0)) :: Expr ('List 'IntTy)
     evalMotive 0 (ListLen lst)
 
   -- proveWith z3 {verbose=True}
 
   -- show that the result of a mapping is all positive
   makeReport "fmap (> 0) lst == true (expect good)" $ do
-    lst <- sList "lst"
-
-    -- the real test is removing this constraint
-    -- constrain $ SBVL.length lst .< 10
-
-    i <- forall "i"
-    constrain $ i .>= 0 ==> lst .!! i .> 0
-
-    let expr  = Sym lst :: Expr ('List 'IntTy)
+    let expr  = ListInfo (CmpInfo (SCmp GT 0)) :: Expr ('List 'IntTy)
         expr' = ListAnd (ListMap (> 0) (.> 0) expr)
 
     evalMotive true expr'
@@ -333,19 +385,22 @@ main = do
 --     evalMotive true expr'
 
   makeReport "(and []) == true (expect good)" $
-    evalMotive true $       ListAnd $ Lit []
+    evalMotive true $       ListAnd $ ListInfo $ LenInfo $ SListLength 0
 
   makeReport "(not (and [])) == true (expect bad)" $
-    evalMotive true $ Not $ ListAnd $ Lit []
+    evalMotive true $ Not $ ListAnd $ ListInfo $ LenInfo $ SListLength 0
 
   makeReport "(and [true]) == true (expect good)" $
-    evalMotive true $       ListAnd $ Lit [true]
+    evalMotive true $       ListAnd $ ListInfo $ AllInfo $ SAll true
+      -- Lit [true]
 
   makeReport "(and [false]) == true (expect bad)" $
-    evalMotive true $       ListAnd $ Lit [false]
+    evalMotive true $       ListAnd $ ListInfo $ AllInfo $ SAll false
+      -- Lit [false]
 
   makeReport "(not (and [false])) == true (expect good)" $
-    evalMotive true $ Not $ ListAnd $ Lit [false]
+    evalMotive true $ Not $ ListAnd $ ListInfo $ AllInfo $ SAll false
+      -- Lit [false]
 
   makeReport "true (expect good)"  $
     constrain true
