72d0e6a3b29cc69c33d9ddb832970d0866b4a39f
Joel Burget <joelburget@gmail.com>
2018-10-16 10:25:18 -0700

    tmp

--- src/Main.hs
+++ src/Main.hs
@@ -38,14 +38,6 @@ type instance Concrete 'BoolTy   = Bool
 type Suitable a = (Show (Concrete a), SymWord (Concrete a), Literal a)
 
 data Expr ty where
-  -- transducers
-  ListCat  :: Suitable a
-           => Expr ('List a) -> Expr ('List a) -> Expr ('List a)
-  ListMap  :: Suitable a
-           => (Expr a -> Expr b)
-           ->                   Expr ('List a) -> Expr ('List b)
-
-  -- producers / information
   -- TODO: use Suitable?
   ListInfo :: HasKind (Concrete a) => ListInfo a -> Expr ('List a)
 
@@ -93,7 +85,7 @@ empty = \case
 data ListInfo a where
   LitList      :: [SBV (Concrete a)]                     -> ListInfo a
   LenInfo      :: Expr 'IntTy                            -> ListInfo a
-  FoldInfo     :: SymWord (Concrete b)
+  FoldInfo :: SymWord (Concrete b)
     -- consuming a list of a, where we operate on bs
     => (Expr a -> Expr b)
     -- fold
@@ -103,10 +95,12 @@ data ListInfo a where
     -> ListInfo a
   AtInfo       :: Expr 'IntTy -> Expr a                  -> ListInfo a
   ContainsInfo :: Expr a                                 -> ListInfo a
+  ListCat      ::              ListInfo a -> ListInfo a  -> ListInfo a
+  MapInfo      :: (Expr a -> Expr b) -> ListInfo b
 
-pattern AllInfo :: (b ~ 'BoolTy) => (Expr a -> Expr b) -> ListInfo a
-pattern AllInfo f <- FoldInfo f And (LitB True) where
-  AllInfo f = FoldInfo f And  (LitB True)
+
+pattern AllInfo :: (Expr a -> Expr b) -> ListInfo 'BoolTy
+pattern AllInfo f = FoldInfo f And (LitB True)
 
 instance HasKind (Concrete a) => Show (ListInfo a) where
   showsPrec p li = showParen (p > 10) $ case li of
@@ -126,22 +120,22 @@ instance HasKind (Concrete a) => Show (ListInfo a) where
       . showsPrec 11 a
     ContainsInfo a -> showString "ContainsInfo " . showsPrec 11 a
 
-instance Show (Expr ty) where
-  showsPrec p expr = showParen (p > 10) $ case expr of
     ListCat a b ->
       showString "ListCat "
       . showsPrec 11 a
       . showString " "
       . showsPrec 11 b
-    ListMap f as ->
-        showString "ListMap "
+    MapInfo f fold as ->
+        showString "MapInfo "
       . showsPrec 11 (f (Sym (uninterpret "x")))
       . showString " "
       . showsPrec 11 as
 
+instance Show (Expr ty) where
+  showsPrec p expr = showParen (p > 10) $ case expr of
     ListInfo i -> showString "ListInfo " . showsPrec 11 i
-    LitB a     -> showString "LitB " . showsPrec 11 a
-    LitI a     -> showString "LitI " . showsPrec 11 a
+    LitB a     -> showString "LitB "     . showsPrec 11 a
+    LitI a     -> showString "LitI "     . showsPrec 11 a
 
     Eq a b ->
         showString "Eq "
@@ -174,9 +168,10 @@ instance Literal 'IntTy where
 
 eval :: Expr ty -> Concrete ty
 eval = \case
-  ListCat a b        -> eval a <> eval b
-  ListMap f as       -> eval . f . lit <$> eval as
-  ListInfo _         -> error "cannot evaluate list info"
+  ListInfo i -> case i of
+    -- ListCat a b  -> eval a <> eval b
+    -- MapInfo f as -> eval . f . lit <$> eval as
+    _            -> error "cannot evaluate list info"
 
   Eq a b             -> eval a == eval b
   Gt a b             -> eval a >  eval b
@@ -192,9 +187,10 @@ eval = \case
 
 sEval :: SymWord (Concrete ty) => Expr ty -> SBV (Concrete ty)
 sEval = \case
-  ListCat a b    -> sEval a .++ sEval b
-  ListMap{}      -> error "can't map with sbv lists"
-  ListInfo{}     -> error "can't evaluate list info"
+  ListInfo i -> case i of
+    -- ListCat a b    -> sEval a .++ sEval b
+    MapInfo{}      -> error "can't map with sbv lists"
+    _              -> error "can't evaluate list info"
 
   Eq a b         -> sEval a .== sEval b
   Gt a b         -> sEval a .>  sEval b
@@ -212,90 +208,84 @@ forAll' :: (SymWord (Concrete a), Provable t) => (Expr a -> t) -> Predicate
 forAll' f = forAll_ $ f . Sym
 
 evalMotive
-  :: forall a. Suitable a
-  => ListInfo a -> Expr ('List a) -> Symbolic (SBV Bool)
-evalMotive _ Sym{} = error "can't motivate evaluation of symbolic value"
+  :: forall a b. Suitable a
+  => ListInfo b -> (Expr a -> Expr b) -> ListInfo a -> Symbolic (SBV Bool)
+evalMotive l1'@(LitList l1) g (MapInfo f l2) =
+  evalMotive l1' (g . f) l2
+  -- pure $ SBVL.implode l1 .== sEval l2'
 
-evalMotive (LitList l1) l2'@(ListMap f l2) = pure $
-  SBVL.implode l1 .== sEval l2'
-
-evalMotive (LenInfo len) (ListCat a b) = do
+evalMotive (LenInfo len) f (ListCat a b) = do
   [al, bl] <- sIntegers ["al", "bl"]
   let totalLen = al + bl .== sEval len
-  aLen <- evalMotive (LenInfo (Sym al)) a
-  bLen <- evalMotive (LenInfo (Sym bl)) b
+  aLen <- evalMotive (LenInfo (Sym al)) f a
+  bLen <- evalMotive (LenInfo (Sym bl)) f b
   pure $ totalLen &&& aLen &&& bLen
-evalMotive (LenInfo len) (ListMap _ lst) = evalMotive (LenInfo len) lst
-evalMotive (LenInfo len) (ListInfo i) =
-  let lenV = sEval len
-  in case i of
-       LenInfo len' -> pure $ sEval len'              .== lenV
-       LitList l    -> pure $ fromIntegral (length l) .== lenV
-       _            -> error $
-         "sorry, can't help with this motive: " ++ show i
+evalMotive (LenInfo len) f (MapInfo g lst)
+  -- we won't evaluate f / g but we have to pass them through so the types
+  -- align
+  = evalMotive (LenInfo len) (f . g) lst
+evalMotive (LenInfo len) _f (LenInfo len')
+  = pure $ sEval len' .== sEval len
 
-evalMotive (FoldInfo mapping fold target) (ListCat a b) = do
+evalMotive (LenInfo len) _f (LitList l)
+  = pure $ fromIntegral (length l) .== sEval len
+
+evalMotive (FoldInfo fold target) f (ListCat a b) = do
   [targetA, targetB] <- mkExistVars 2
   let append = BinOp fold
   constrain $ sEval target .== sEval (Sym targetA `append` Sym targetB)
   (&&&)
-    <$> evalMotive (FoldInfo mapping fold (Sym targetA)) a
-    <*> evalMotive (FoldInfo mapping fold (Sym targetB)) b
+    <$> evalMotive (FoldInfo fold (Sym targetA)) f a
+    <*> evalMotive (FoldInfo fold (Sym targetB)) f b
 
 -- g is at least as strong an assumption as f.
 -- example:
 -- f: for all i: elements of the list, i > 0
 -- g: i need to know that for all i: elements of the list, i > -1
-evalMotive (FoldInfo f And target) (ListInfo (FoldInfo g And lst))
+evalMotive (FoldInfo And target) f (FoldInfo And lst)
   = forAll' $ \j ->
-    sEval (f j `Eq` target)
+    sEval (j `Eq` target)
     ==>
-    sEval (g j `Eq` lst)
-evalMotive (FoldInfo f Add target) (ListInfo (FoldInfo g Add lst))
+    sEval (j `Eq` lst)
+evalMotive (FoldInfo Add target) f (FoldInfo Add lst)
   = forAll' $ \j ->
-    sEval (f j `Eq` target)
+    sEval (j `Eq` target)
     ==>
-    sEval (g j `Eq` lst)
+    sEval (j `Eq` lst)
 
-evalMotive (FoldInfo mapping fold target) (ListInfo info) = case (fold, info) of
-  (_, LenInfo len)
-    | Just cLen <- unliteral (sEval len) -> do
-      vars <- mkExistVars (fromIntegral cLen)
-      pure $
-        sEval (foldr (BinOp fold) (empty fold) (fmap (mapping . Sym) vars))
-        .==
-        sEval target
-    | otherwise -> error "TODO: folding non-literal length list"
-  (_, LitList l) -> pure $
+evalMotive (FoldInfo fold target) f (LenInfo len)
+  | Just cLen <- unliteral (sEval len) = do
+    vars <- mkExistVars (fromIntegral cLen)
+    pure $
+      sEval (foldr (BinOp fold) (empty fold) (f . Sym <$> vars))
+      .==
+      sEval target
+  | otherwise = error "TODO: folding non-literal length list"
+
+evalMotive (FoldInfo fold target) f (LitList l) = pure $
     sEval target
     .==
-    sEval (foldr (BinOp fold) (empty fold) (fmap (mapping . Sym) l))
-  _ -> error $ "sorry, can't help with this motive: " ++ show info
-evalMotive (FoldInfo mapping fold target) (ListMap g lst) =
-  evalMotive (FoldInfo (mapping . g) fold target) lst
+    sEval (foldr (BinOp fold) (empty fold) (f . Sym <$> l))
+evalMotive (FoldInfo fold target) f (MapInfo g lst) =
+  evalMotive (FoldInfo fold target) (f . g) lst
 
-evalMotive FoldInfo{} _ = error "sorry, can't help with this motive"
+evalMotive FoldInfo{} _f _ = error "sorry, can't help with this motive"
 
-evalMotive (AtInfo i a) (ListCat l1 l2) = do
-    let l1' = sEval l1
-        l2' = sEval l2
-        i'  = sEval i
-        a'  = sEval a
-    pure $
-      l1' .!! i'                     .== a' |||
-      l2' .!! (i' - SBVL.length l1') .== a'
-evalMotive (AtInfo i _) (ListMap _ lst) = evalMotive (AtInfo i (error "TODO")) lst
-evalMotive (AtInfo i a) (ListInfo info) = case info of
-  LitList litList -> pure $
-    let iV = sEval i
-        aV = sEval a
-    in fromIntegral (length litList) .> iV &&&
-       (SBVL.implode litList SBVL..!! iV) .== aV
+evalMotive (AtInfo i a) f (ListCat l1 l2) = do
+  case1 <- evalMotive (AtInfo i a) f l1
+  error "TODO"
+  -- let l1' = sEval l1
+  --     i'  = sEval i
+  -- case2 <- evalMotive (AtInfo (Sym (i' - SBVL.length l1')) a) f l2
+  -- pure $ case1 ||| case2
 
-  _ -> error "can't help with this motive"
+evalMotive m@AtInfo{} f (MapInfo g lst) = evalMotive m (f . g) lst
+evalMotive (AtInfo i a) f (LitList litList) = pure $
+  let iV = sEval i
+      aV = sEval a
+  in fromIntegral (length litList) .> iV &&&
+     (SBVL.implode (fmap (error "TODO") litList) SBVL..!! iV) .== aV
 
-evalMotive LenInfo{} BinOp{} = error "vacuous match"
-evalMotive AtInfo{}  BinOp{} = error "vacuous match"
 
 gt0 :: Expr 'IntTy -> Expr 'BoolTy
 gt0 x = Gt x 0
@@ -303,15 +293,15 @@ gt0 x = Gt x 0
 lte0 :: Expr 'IntTy -> Expr 'BoolTy
 lte0 x = Not (Gt x 0)
 
-mkAnyM :: (Expr a -> Expr 'BoolTy) -> ListInfo a
-mkAnyM f = FoldInfo (bnot . f) And false
+pattern AnyM :: (Expr a -> Expr 'BoolTy) -> ListInfo a
+pattern AnyM f = FoldInfo (MapInfo f) And (LitB False)
 
 mkTest
-  :: forall a. Suitable a
-  => Validity -> ListInfo a -> Symbolic (Expr ('List a)) -> Test ()
-mkTest expectValid motive expr = do
+  :: forall a b. Suitable a
+  => Validity -> ListInfo b -> (Expr a -> Expr b) -> Symbolic (ListInfo a) -> Test ()
+mkTest expectValid motive f expr = do
   (valid, vacuous) <- io $ do
-    let constraints = constrain =<< evalMotive motive =<< expr
+    let constraints = constrain =<< evalMotive motive f =<< expr
     (,) <$> isTheorem constraints <*> isVacuous constraints
 
   expect $ if expectValid == Valid
@@ -323,98 +313,98 @@ data Validity = Valid | Invalid
 
 main :: IO ()
 main = do
-  let almostAllPos :: Expr ('List 'IntTy)
+  let almostAllPos :: ListInfo 'IntTy
       almostAllPos = ListCat
-        (ListInfo (AllInfo gt0))
+        (AllInfo gt0)
         (ListCat
-          (ListInfo (AllInfo (Eq 0)))
-          (ListInfo (AllInfo gt0)))
+          (AllInfo (Eq 0))
+          (AllInfo gt0))
 
-      sumTo7 :: Expr ('List 'IntTy)
+      sumTo7 :: ListInfo 'IntTy
       sumTo7 = ListCat
-        (ListInfo (FoldInfo id Add 3))
-        (ListInfo (FoldInfo id Add 4))
+        (FoldInfo Add 3)
+        (FoldInfo Add 4)
 
   run $ tests
     [ scope "any (> 0) [1, 2, 3]" $
-        mkTest Valid (mkAnyM gt0) $ pure $ ListInfo $ LitList [1, 2, 3]
+        mkTest Valid AnyM (bnot . gt0) $ pure $ ListInfo $ LitList [1, 2, 3]
     , scope "any (> 0) [-1, 2, 3]" $
-        mkTest Valid (mkAnyM gt0) $ pure $ ListInfo $ LitList [-1, 2, 3]
+        mkTest Valid AnyM (bnot . gt0) $ pure $ ListInfo $ LitList [-1, 2, 3]
 
     , scope "any (> 0) [a, -1, 3]" $ do
-        mkTest Valid (mkAnyM gt0) $ do
+        mkTest Valid AnyM (bnot . gt0) $ do
           a <- sInteger "a"
           pure $ ListInfo $ LitList [a, -1, 3]
 
     , scope "all (> 0) [a, -1, 3]" $
-      mkTest Invalid (FoldInfo gt0 And true) $ do
+      mkTest Invalid (FoldInfo And true) gt0 $ do
         a <- sInteger "a"
         pure $ ListInfo $ LitList [a, -1, 3]
 
     , scope "length [] == 0" $ do
         let lst = ListInfo (LenInfo 0) :: Expr ('List 'IntTy)
-        mkTest Valid (LenInfo 0) $ pure lst
+        mkTest Valid (LenInfo 0) id $ pure lst
 
     , scope "length (len 2) == 2" $ do
         let lst :: Expr ('List 'IntTy)
             lst = ListInfo (LenInfo 2)
-        mkTest Valid (LenInfo 2) $ pure lst
+        mkTest Valid (LenInfo 2) id $ pure lst
 
       -- show that the result of a mapping is all positive
     , scope "fmap (> 0) lst == true" $ do
         let lst = ListInfo (AllInfo gt0) :: Expr ('List 'IntTy)
-        mkTest Valid (FoldInfo gt0 And true) $ pure lst
+        mkTest Valid (FoldInfo And true) gt0 $ pure lst
 
     , scope "fmap (> 0) almostAllPos == true" $
-        mkTest Invalid (FoldInfo gt0 And true) $ pure almostAllPos
+        mkTest Invalid (FoldInfo And true) gt0 $ pure almostAllPos
 
     , scope "(and []) == true" $
-        mkTest Valid (FoldInfo (Eq @'BoolTy true) And true)
+        mkTest Valid (FoldInfo And true) (Eq @'BoolTy true)
           $ pure $ ListInfo $ LenInfo 0
 
     , scope "all (eq true) [] /= false" $ do
-        mkTest Invalid (FoldInfo (Eq @'BoolTy true) And false)
+        mkTest Invalid (FoldInfo And false) (Eq @'BoolTy true)
           $ pure $ ListInfo $ LenInfo 0
 
     , scope "(and [true]) == true" $
-        mkTest @'BoolTy Valid (FoldInfo (Eq true) And true)
+        mkTest @'BoolTy Valid (FoldInfo And true) (Eq true)
           $ pure $ ListInfo $ AllInfo $ Eq $ Sym true
 
     , scope "(and [false]) == true" $
         mkTest Invalid
-          (FoldInfo (Eq true) And true)
+          (FoldInfo And true) (Eq true)
           $ pure $ ListInfo $ FoldInfo id And false
 
     , scope "and [false] /= true" $
         mkTest Valid
-          (FoldInfo (Eq true) And false)
+          (FoldInfo And false) (Eq true)
           $ pure $ ListInfo $ FoldInfo id And false
 
     , scope "all (> 0) => (not (any (> 0)) == false)" $
         mkTest Invalid
-          (FoldInfo gt0 And false)
+          (FoldInfo And false) gt0
           $ pure $ ListInfo $ AllInfo gt0
 
     , scope "any (<= 0) => not (all (> 0))" $
         mkTest Valid
-          (FoldInfo gt0 And false)
+          (FoldInfo And false) gt0
           $ pure $ ListInfo $ FoldInfo (bnot . lte0) And false
 
     , scope "at 2 [1, 2, 3] == 3" $
         mkTest Valid
-          (AtInfo (LitI 2) (LitI 3))
+          (AtInfo (LitI 2) (LitI 3)) id
           $ pure $ ListInfo $ LitList [1, 2, 3]
 
     , scope "at 2 [1, 2, 3] == 2" $
         mkTest Invalid
-          (AtInfo (LitI 2) (LitI 2))
+          (AtInfo (LitI 2) (LitI 2)) id
           $ pure $ ListInfo $ LitList [1, 2, 3]
 
     , scope "sum sumTo7 == 7" $
-        mkTest Valid (FoldInfo id Add 7) $ pure sumTo7
+        mkTest Valid (FoldInfo Add 7) id $ pure sumTo7
 
     , scope "sum (map (const 1) [length 7]) == 7" $ do
         let lst :: Expr ('List 'IntTy)
             lst = ListInfo (LenInfo 7)
-        mkTest Valid (FoldInfo (const 1) Add 7) $ pure lst
+        mkTest Valid (FoldInfo Add 7) (const 1) $ pure lst
     ]
